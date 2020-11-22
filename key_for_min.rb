61c7a6da3f6792e375102cd6b8a2cb5f8353d16b
Dan <DanielMooreRealPerson@gmail.com>
2020-11-22 17:06:52 -0500

    commit tree d172b5f0d9c89cb7bf18c16bf40cfe6b981db7c1

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+  nil if name_hash.length == 0
+    lowest = {}
+    name_hash.each do |name, price|
+        if lowest.length == 0 
+            lowest[:name] = name
+            lowest[:price] = price
 
+        elsif lowest[:number] > number
+            lowest[:name] = name
+            lowest[:number] = number
+        end
+    end
+    lowest[:name]
 end
+            
